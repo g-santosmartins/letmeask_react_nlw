8e91d1423f0f5ee7b3e4f40a5e13825353f2ac73
Guilherme Martins <g.santosmartins@outlook.com>
2021-06-22 12:01:35 -0300

    Finalizando criação e alinhamento de Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,13 +13,14 @@ export function Home() {
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="logo da letmeask" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>
+          <div className="separator">Ou entre em uma sala</div>
+          <form>
             <input
               type="text"
               placeholder="Digite o código da sala"
@@ -27,7 +28,7 @@ export function Home() {
             <button type="submit">
               Entrar na sala
             </button>
-          </div>
+          </form>
         </div>
       </main>
     </div>
